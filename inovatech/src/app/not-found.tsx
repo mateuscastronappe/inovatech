1fb9678c9bc6f2656f1c8c373850ae2763f4f41e
mateuscastronappe <teteucastro31@gmail.com>
2024-11-12 23:15:45 -0300

    criando a página not found

---
+++ inovatech/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div className="textoNotFound">
+          <h1>404 : Not Found</h1>
+          <p>Página não encontrada!</p>
+      </div>
+    )
+  }
