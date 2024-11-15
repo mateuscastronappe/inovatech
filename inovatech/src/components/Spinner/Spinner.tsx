eb3e69c3ac38949a280ee8509f4c9fe3228c6c16
mateuscastronappe <teteucastro31@gmail.com>
2024-11-15 18:10:42 -0300

    colocando o spinner verde

--- inovatech/src/components/Spinner/Spinner.tsx
+++ inovatech/src/components/Spinner/Spinner.tsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
     return (
       <div className="flex justify-center items-center">
-          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-600"></div>
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-green-400"></div>
       </div>
     )
   }
