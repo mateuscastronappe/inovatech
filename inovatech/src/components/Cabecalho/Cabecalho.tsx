2c18dfad22e20f4f316b740a129c2c094078b292
mateuscastronappe <teteucastro31@gmail.com>
2024-11-15 15:47:26 -0300

    criado cabeçalho e estilizado no globals css

--- inovatech/src/components/Cabecalho/Cabecalho.tsx
+++ inovatech/src/components/Cabecalho/Cabecalho.tsx
@@ -1,8 +1,13 @@
+import Menu from "@/components/Menu/Menu";
+import Image from "next/image";
+import imgLogo from "@/img/logo-energia.jpg"
 
 export default function Cabecalho() {
     return (
-        <header> 
-            
+        <header className="cabecalho">
+            <Image src={imgLogo} alt="logo" width={40} height={40} className="mr-2 mt-2 inline-flex" />
+            <h1>Verdis</h1>
+            <Menu />
         </header>
-    )
+    );
 }
