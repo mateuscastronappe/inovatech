87688a5dc50e0cf3b06dddb854110910ec19a70d
mateuscastronappe <teteucastro31@gmail.com>
2024-11-15 18:10:32 -0300

    estilizando o texto not found

--- inovatech/src/app/not-found.tsx
+++ inovatech/src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
     return (
-      <div className="textoNotFound">
+      <div className="textoNotFound bg-gradient-to-l from-green-400 via-green-500 to-green-600 text-transparent bg-clip-text text-[40px] text-center">
           <h1>404 : Not Found</h1>
           <p>Página não encontrada!</p>
       </div>
