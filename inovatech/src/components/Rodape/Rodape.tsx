d0973ac679a571d90aeb0a7df9da2e10b354daad
mateuscastronappe <teteucastro31@gmail.com>
2024-11-15 15:47:55 -0300

    criando o rodapé

---
+++ inovatech/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,8 @@
+
+export default function Rodape() {
+    return (
+        <footer className="rodape text-3xl p-12 flex justify-center text-green-500">
+            <p>Desenvolvido por <span>InovaTech</span>&copy; 2024</p>
+        </footer>
+    )
+}
